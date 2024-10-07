b4dffca7fd437811cb0dffbdb6ba2c6df22ebe03
Jones Pedrosa <142640703+jonespedrosa@users.noreply.github.com>
2024-10-07 16:23:29 +0800

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace Digital_Persona_Fingerprint
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StatusText = new System.Windows.Forms.Label();
+            this.fingerImage = new System.Windows.Forms.PictureBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.fingerImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // StatusText
+            // 
+            this.StatusText.AutoSize = true;
+            this.StatusText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StatusText.Location = new System.Drawing.Point(17, 361);
+            this.StatusText.Name = "StatusText";
+            this.StatusText.Size = new System.Drawing.Size(56, 20);
+            this.StatusText.TabIndex = 0;
+            this.StatusText.Text = "Status";
+            // 
+            // fingerImage
+            // 
+            this.fingerImage.Location = new System.Drawing.Point(22, 12);
+            this.fingerImage.Name = "fingerImage";
+            this.fingerImage.Size = new System.Drawing.Size(533, 304);
+            this.fingerImage.TabIndex = 1;
+            this.fingerImage.TabStop = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(18, 322);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(537, 35);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(567, 410);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.fingerImage);
+            this.Controls.Add(this.StatusText);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Main Form";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.fingerImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label StatusText;
+        private System.Windows.Forms.PictureBox fingerImage;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Digital_Persona_Fingerprint
+{
+    public partial class MainForm : Form, DPFP.Capture.EventHandler
+    {
+        private DPFP.Capture.Capture Capturer;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        protected void MakeReport(string message)
+        {
+            this.Invoke(new Action(delegate ()
+            {
+                StatusText.Text = message;
+            }));
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                Capturer = new DPFP.Capture.Capture();
+                if (Capturer != null)
+                {
+                    Capturer.EventHandler = this;
+                    MakeReport("Press start capture to start scanning");
+                } 
+                else
+                {
+                    MakeReport("Cannot initiate capture operation");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Can't initiate capture operation!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public void OnReaderConnect(object Capture, string ReaderSerialNumber)
+        {
+            MakeReport("The fingerprint reader was connected");
+        }
+
+        public void OnReaderDisconnect(object Capture, string ReaderSerialNumber)
+        {
+            MakeReport("The fingerprint reader was disconnected");
+        }
+
+        public void OnFingerGone(object Capture, string ReaderSerialNumber)
+        {
+            MakeReport("The fingerprint was removed from the fingerprint reader");
+        }
+
+        public void OnFingerTouch(object Capture, string ReaderSerialNumber)
+        {
+            MakeReport("The fingerprint reader was touched");
+        }
+
+        public void OnComplete(object Capture, string ReaderSerialNumber, DPFP.Sample Sample)
+        {
+            MakeReport("The fingerprint reader was captured");
+            Process(Sample);
+        }
+
+        public void OnSampleQuality(object Capture, string ReaderSerialNumber, DPFP.Capture.CaptureFeedback CapturedFeedback)
+        {
+            if (CapturedFeedback == DPFP.Capture.CaptureFeedback.Good)
+                MakeReport("The quality of the fingerprint sample is good.");
+            else
+                MakeReport("The quality of the fingerprint sample is poor.");
+        }
+
+        protected virtual void Process(DPFP.Sample Sample)
+        {
+            DrawPicture(ConvertSampleToBitmap(Sample));
+        }
+
+        protected Bitmap ConvertSampleToBitmap(DPFP.Sample Sample)
+        {
+            DPFP.Capture.SampleConversion Convertor = new DPFP.Capture.SampleConversion();
+            Bitmap bitmap = null;
+            Convertor.ConvertToPicture(Sample, ref bitmap);
+
+            return bitmap;
+        }
+
+        private void DrawPicture(Bitmap bitmap)
+        {
+            fingerImage.Image = new Bitmap(bitmap, fingerImage.Size);
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if(Capturer != null)
+            {
+                try
+                {
+                    Capturer.StartCapture();
+                           MakeReport("Using the fingerprint reader, scan your fingerprint.");
+                }
+                catch
+                {
+                    MakeReport("Can't initiate captured.");
+                }
+            }
+        }
+    }
+}
